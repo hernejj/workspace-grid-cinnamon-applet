7f741426981fb97579fe8d8460ae83112e5a78dd
Jason J. Herne <hernejj@gmail.com>
2014-08-14 08:20:29 -0400

    more robust support for correcting external desktop add/removes.

--- workspace-grid@hernejj/WorkspaceController.js
+++ workspace-grid@hernejj/WorkspaceController.js
@@ -8,16 +8,25 @@ function WorkspaceController(cols, rows) {
 WorkspaceController.prototype = {
     
     _init: function(cols, rows) {
+        this.watchForUnexpectedWorkspaceChanges(true);
         this.set_workspace_grid(cols, rows);
-        this.onNumWorkspacesChangedID = global.screen.connect('notify::n-workspaces', Lang.bind(this, this.numWorkspacesChanged));
     },
      
     // Create proper workspace layout geometry within Gnome
     set_workspace_grid: function (cols, rows) {
         this.cols = cols;
         this.rows = rows;
+        this.watchForUnexpectedWorkspaceChanges(false);
         this.__equalize_num_workspaces();
         global.screen.override_workspace_layout(Meta.ScreenCorner.TOPLEFT, false, rows, cols);
+        this.watchForUnexpectedWorkspaceChanges(true);
+    },
+    
+    watchForUnexpectedWorkspaceChanges: function (watch) {
+        if (watch)
+            this.onNumWorkspacesChangedID = global.screen.connect('notify::n-workspaces', Lang.bind(this, this.numWorkspacesChanged));
+        else
+            global.screen.disconnect(this.onNumWorkspacesChangedID);
     },
     
     // Update Gnome's view of workspaces to reflect our count based on row*col.
@@ -37,18 +46,18 @@ WorkspaceController.prototype = {
         }
     },
     
+    // If a desktop was added or removed external to this applet, then numRows and numCols
+    // are not updated to reflect its existence. This is bad! We can detect this case
+    // and correct it by undoing the add/remove.
     numWorkspacesChanged: function() {
-        // If this desktop was added external to this applet, then numRows and numCols
-        // are not updated to reflect its existence. This is bad! We can detect this case
-        // and correct it by removing the additional desktop
-        if (this.rows*this.cols < global.screen.n_workspaces) {
+        if (this.rows*this.cols != global.screen.n_workspaces) {
             this.__equalize_num_workspaces();
         }
     },
     
     // This applet is going away. Revert to allowing Cinnamon to control workspaces.
     release_control: function() {
-        global.screen.disconnect(this.onNumWorkspacesChangedID);
+        this.watchForUnexpectedWorkspaceChanges(false);
         this.set_workspace_grid(-1, 1); // Set to no rows, and a single desktop
     }
 };
